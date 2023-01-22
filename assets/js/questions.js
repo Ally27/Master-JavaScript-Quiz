cb2ba645feca561b46ef8bc53bb6d5b276131c99
Allyson Gonzales <Allysonmg21@gmail.com>
2023-01-22 13:09:17 -0800

    created questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+//creating question array
+var questions = [
+  {
+    question: "What data type returns true or false?",
+    answer: ["number", "boolean", "string", "if else"],
+    correct: 1,
+  },
+  {
+    question: "What is the most used programing language in 2023?",
+    answers: ["Java", "C/C++", "Python", "JavaScript"],
+    correct: 2,
+  },
+  {
+    question: "What year was JavaScript invented?",
+    answers: ["1967", "2001", "1980", "None of the above"],
+    correct: 3,
+  },
+  {
+    question: "Which keyword is NOT used to declare a variable?",
+    answers: ["let", "const", "store", "var"],
+    correct: 3,
+  },
+];
