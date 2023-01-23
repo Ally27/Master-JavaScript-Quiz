55d97170d8d773288c3eab8c503b4501f7ca2f27
Allyson Gonzales <Allysonmg21@gmail.com>
2023-01-22 18:38:09 -0800

    created objects by correct/time values

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,2 +1,26 @@
 var highscoreList = document.getElementById("highscoreList");
-console.log(highscoreList);
+
+var clearHistoryEl = document.getElementById("clearHistory");
+var storedHighscores =
+  JSON.parse(localStorage.getItem("storedHighscores")) || [];
+console.log(storedHighscores);
+
+//stores objects
+storedHighscores.sort(function (a, b) {
+  var answerA = a.answerScore;
+  var timeA = a.timeScore;
+  var answerB = b.answerScore;
+  var timeB = b.timeScore;
+
+  return answerA < answerB
+    ? 1
+    : answerA > answerB
+    ? -1
+    : answerA === answerB
+    ? timeA < timeB
+      ? 1
+      : timeA > timeB
+      ? -1
+      : 0
+    : null;
+});
